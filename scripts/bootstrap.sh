60ca1388d570382b0cc7d61e62f3ee5199991403
Mirko <mirko@cloudera.com>
2015-09-25 17:12:39 +0200

    changed bootstrap.sh

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,10 +1,16 @@
-#
-# bootstrapper to install gnuplot, Java8 and wget on CentOS VMs
-#
-
 
 mkdir bootstrap
 cd bootstrap
+
+#
+# This BOOTSTRAPER installs Libraries for MorphMiner2 into the mvn-repo
+#
+#-------------------------------------------------------------------------------
+mvn install:install-file -Durl=file:///home/cloudera/MorphMiner/lib -Dfile=lib/sshxcute-1.0.jar -DgroupId=net.neoremind.sshxcute -DartifactId=sshxcute -Dpackaging=jar -Dversion=1.0
+mvn install:install-file -Durl=file:///home/cloudera/MorphMiner/lib -Dfile=lib/json-lib.jar -DgroupId=net.sf.json-lib -DartifactId=json-lib -Dpackaging=jar -Dversion=2.4
+
+
+
 
 #-------------------
 #
@@ -16,8 +22,8 @@ cd bootstrap
 
 #wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u60-b27/jdk-8u60-linux-x64.tar.gz"
 #tar xzf jdk-8u60-linux-x64.tar.gz
-#sudo mv jdk1.8.0_60 /opt/
-sudo ln -s java8 /opt/jdk1.8.0_60 java8
+sudo mv jdk1.8.0_60 /opt/
+sudo ln -s /opt/jdk1.8.0_60 /opt/java8
 
 sudo alternatives --install /usr/bin/java java /opt/java8/bin/java 2
 sudo alternatives --config java
@@ -30,6 +36,11 @@ sudo alternatives --set javac /opt/java8/bin/javac
 
 export JAVA_HOME=/opt/java8
 
+
+
+#
+#  If not yet available we install Gnuplot and wget
+#
 sudo yum install gnuplot
 sudo yum install wget
 
